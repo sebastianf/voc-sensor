c3ace9fb2b3b31b9b03c87f5f183340fcb2ba1b3
Sebastian Friedrich <sebastian.friedrich@mac.com>
2021-01-05 22:32:42 +0100

    adds sketchbook using the ccs811 adafrut library

--- TempVOCDisplay/TempVOCDisplayStandard.ino
+++ TempVOCDisplay/TempVOCDisplayStandard.ino
@@ -6,12 +6,15 @@
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "Adafruit_CCS811.h"
+#include <Adafruit_Sensor.h>
 #include <Wire.h>    // I2C library
 #include <DHT.h>
-#include "ccs811.h"  // CCS811 library
 
 #define DHTTYPE   DHT22
 #define DHTPIN    12
+
+#define I2C_CCS811_ADDRESS 0x5A    // CCS811 I2C address
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -19,13 +22,12 @@
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_CCS811 ccs;                     // CCS811 is connected (I2C) to D1-->SLC-->GPIO5-->Pin5 & D2-->SDA-->GPIO4-->Pin4
 
 DHT dht(DHTPIN, DHTTYPE, 22);
-CCS811 ccs811(D3); // nWAKE on D3
 
 float temperature, humidity;
 int soil = analogRead(A0);
-unsigned long previousMillis = 0;
 const long UPDATE_INTERVAL_SECONDS = 2;
 float humidityThreshold = 60;
 uint16_t eco2Threshold = 1500; // up to 1000 is ok over 2000 is bad, fresh air is needed
@@ -42,25 +44,24 @@ void setup() {
   } 
 
   // Enable I2C, e.g. for ESP8266 NodeMCU boards: VDD to 3V3, GND to GND, SDA to D2, SCL to D1, nWAKE to D3 (or GND)
-  Serial.print("setup: I2C ");
   Wire.begin(); 
-  Serial.println("ok");
-  
   // Enable DHT22
-  Serial.print("setup: DHT22 ");
   dht.begin();
   humidity = dht.readHumidity();
   temperature = dht.readTemperature();
   pinMode(soil, INPUT);
 
   // Enable CCS811
-  Serial.print("setup: CCS811 ");
-  ccs811.set_i2cdelay(50); // Needed for ESP8266 because it doesn't handle I2C clock stretch correctly
-  ccs811.begin();
+  ccs.begin();                            // Enable CCS811
+  delay(10);
+  Serial.println("CCS811 test");
+  if(!ccs.begin()){
+    Serial.println("Failed to start CCS811 sensor! Please check your wiring!");
+    while(1);
+  }
 
-  // Start CCS811 (measure every 1 second)
-  Serial.print("setup: CCS811 start ");
-  ccs811.start(CCS811_MODE_1SEC);
+  // Set CCS811 to Mode 2: Pulse heating mode IAQ measurement every 10 seconds
+  ccs.setDriveMode(CCS811_DRIVE_MODE_1SEC);
 
   // Show initial display buffer contents on the screen --
   // the library initializes this with an Adafruit splash screen.
@@ -70,13 +71,10 @@ void setup() {
   display.clearDisplay();
 
   pinMode(D5,OUTPUT);  //red led
-
 }
 
 
 void loop() {
-  unsigned long currentMillis = millis();
-
   humidity = dht.readHumidity();
   temperature = dht.readTemperature();
                  
@@ -86,62 +84,48 @@ void loop() {
   }
   Serial.println("Reporting " + String((int)temperature) + "C and " + String((int)humidity) + " % humidity");
 
-  Serial.println();
-        //clear display
-    display.clearDisplay();
-   
-    // display temperature
-    display.setTextSize(1);
-    display.setTextColor(SSD1306_WHITE);
-    display.setCursor(0,0);
-    display.print("Temperature: ");
-    display.setTextSize(2);
-    display.setCursor(0,10);
-    display.print(temperature);
-    display.print(" ");
+  //clear display
+  display.clearDisplay();
+ 
+  // display temperature
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  display.print("Temperature: ");
+  display.setTextSize(2);
+  display.setCursor(0,10);
+  display.print(temperature);
+  display.print(" ");
 
-    display.print("C");
-    
-    // display humidity
-    display.setTextSize(1);
-    display.setCursor(0, 30);
-    display.print("Humidity: ");
-    display.setTextSize(2);
-    display.setCursor(0, 40);
-    display.print(humidity);
-    display.print(" %");
-    display.setTextSize(1);
-    display.setCursor(0, 55);
+  display.print("C");
+  
+  // display humidity
+  display.setTextSize(1);
+  display.setCursor(0, 30);
+  display.print("Humidity: ");
+  display.setTextSize(2);
+  display.setCursor(0, 40);
+  display.print(humidity);
+  display.print(" %");
+  display.setTextSize(1);
+  display.setCursor(0, 55);
 
 
-  ccs811.set_envdata(temperature, humidity);
-  Serial.print("  ");
+  // Pass DHT22 temp & hum readings to CSS811 for compensation algorithm
+  ccs.setEnvironmentalData(humidity, temperature);
 
   // Read CCS811
-  uint16_t eco2, etvoc, errstat, raw;
-  ccs811.read(&eco2,&etvoc,&errstat,&raw); 
-
-  // Process CCS811
-  Serial.print("CCS811: ");
-  if( errstat==CCS811_ERRSTAT_OK ) {
-    Serial.print("eco2=");  Serial.print(eco2);  Serial.print(" ppm  ");
-    Serial.print("etvoc="); Serial.print(etvoc); Serial.print(" ppb  "); 
-    display.print(eco2); display.print("ppm ");
-    display.print(etvoc); display.print("ppb");
- 
-  } else if( errstat==CCS811_ERRSTAT_OK_NODATA ) {
-    Serial.print("waiting for (new) data");
-    display.print("waiting for (new) data");
-  } else if( errstat & CCS811_ERRSTAT_I2CFAIL ) { 
-    Serial.print("I2C error");
-    display.print("I2C error");
-  } else {
-    Serial.print( "error: " );
-    Serial.print( ccs811.errstat_str(errstat) );
-    display.print( "error: " );
-    display.print( ccs811.errstat_str(errstat) );
-  }
-    
+  if(ccs.available()){
+    float temp = ccs.calculateTemperature();
+    if(ccs.readData()){
+      // Read CCS811 values
+      float eco2 = ccs.geteCO2();
+      float tvoc = ccs.getTVOC();
+      Serial.println("eco2= " + String((float)eco2) + "ppm tvoc=" + String((float)tvoc) + "ppb");
+      display.print(eco2); display.print("ppm ");
+      display.print(tvoc); display.print("ppb");
+    }
+  }    
   display.display();
 
 //  if (humidity > humidityThreshold || eco2 > eco2Threshold || etvoc > etvocThreshold) {
